f15058df026fe21669383130989bafac568f580e
Jesse Jaggars <jjaggars@redhat.com>
2019-04-15 17:39:26 -0500

    running forever now
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- logripper.py
+++ logripper.py
@@ -1,10 +1,14 @@
+import time
 import logging
 from itertools import count
 
 logga = logging.getLogger("__name__")
 logging.basicConfig(level=logging.INFO)
 
-for i in count():
-    logga.info("Up up up! %s", i)
-    if i > 1_000_000:
-        break
+
+while True:
+    for i in count():
+        logga.info("Up up up! %s", i)
+        if i > 1_000_000:
+            break
+    time.sleep(3600)
